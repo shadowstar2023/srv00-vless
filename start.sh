8d7076a6a4f9e6ae64a8c72ee53c9c25a30c41ff
shadowstar2023 <imshadowstar@gmail.com>
2024-01-05 04:57:57 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="abc.xyz:5555"
-export NEZHA_KEY="abcdwfghijklmnopqrstuvwxyz"
+export NEZHA_SERVER="nezha.130136.xyz:443"
+export NEZHA_KEY="6Kf5QzDNLwaaPwSqxq"
 
 chmod +x swith start.sh
-nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
+nohup ./swith -s ${NEZHA_SERVER} -p ${NEZHA_KEY} --tls > /dev/null 2>&1 &   #需要tls在 > 前面加上 --tls即可
 
 tail -f /dev/null
